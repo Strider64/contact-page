c2186f6860d897feca493d8ef06fb1d9c400cd44
John Pepp <jrpepp@pepster.com>
2016-10-11 00:17:48 -0400

    Create connect.php

---
+++ connect/connect.php
@@ -0,0 +1,7 @@
+<?php
+
+define('EMAIL_HOST', 'email_host_string');
+define('EMAIL_USERNAME', 'email_username');
+define('EMAIL_PASSWORD', 'email_password');
+define('EMAIL_ADDRESS', 'your_email_address');
+define('EMAIL_PORT', 587);
